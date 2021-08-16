8b574844a7de9510c9f3920f75e187ee9b70fd07
steve zhang <871506263@qq.com>
2021-08-16 18:20:57 +0800

    submit 006

--- examples/main.js
+++ examples/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue';
-import ElementUI from 'element-ui'; // 导入组件库
-import 'element-ui/lib/theme-chalk/index.css'; // 导入组件库样式
+import ElementUI from 'element-ui'; // 导入element组件库
+import 'element-ui/lib/theme-chalk/index.css'; // 导入element组件库样式
+import zjhui from 'zjh-vue-ui'; // 导入zjhui组件库
+import 'zjh-vue-ui/libs/zjhui.css'; // 导入zjhui组件库样式
 import App from './App.vue';
 import router from './router';
 import store from './store';
-Vue.use(ElementUI); // 注册组件库
+Vue.use(ElementUI); // 注册组件库: 全局导入
+Vue.use(zjhui); // 注册组件库: 全局导入
 
 // import zjhui from '../packages/index'; // 导入组件库 全局导入
-import { ZjhButton, ZjhInput } from '../packages/index'; // 导入组件库: 按需导入
+// import { ZjhButton, ZjhInput } from '../packages/index'; // 导入组件库: 按需导入
 // Vue.use(zjhui); // 注册组件库: 全局导入
-Vue.use(ZjhButton); // 注册组件库: 按需导入
-Vue.use(ZjhInput); // 注册组件库: 按需导入
+// Vue.use(ZjhButton); // 注册组件库: 按需导入
+// Vue.use(ZjhInput); // 注册组件库: 按需导入
 
 Vue.config.productionTip = false;
 
